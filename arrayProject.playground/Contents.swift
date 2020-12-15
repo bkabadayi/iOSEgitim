d3b5a515ea00d6f1602e1efd721968113c52fdd7
bkabadayi <berkinkabadayi@gmail.com>
2020-12-15 15:13:24 +0300

    Creating odd and even number array and printing

--- arrayProject.playground/Contents.swift
+++ arrayProject.playground/Contents.swift
@@ -1,5 +1,18 @@
 var numberArray: [Int] = []
+var numberEvenArray: [Int] = []
+var numberOddArray: [Int] = []
+var evenSum: Int = 0
+var oddSum: Int = 0
+
 for item in 0...999 {
     numberArray.append(item+1)
+    if numberArray[item] % 2 == 0 {
+        numberEvenArray.append(item+1)
+    }
+    else {
+        numberOddArray.append(item+1)
+    }
 }
-print("\(numberArray) \n")
+print("Even numbers are: \(numberEvenArray) \n ")
+print("Odd numbers are: \(numberOddArray) \n ")
+
